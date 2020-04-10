6efa3b8d8ff3b68eef15fe8366d6c5c76984e01b
matt <->
2020-04-10 17:34:45 +0200

    C : Removed old struct type macro

--- src/struct-type.lisp
+++ src/struct-type.lisp
@@ -19,15 +19,3 @@
 (defmacro defstruct-type-function (parent-type struct-name slot)
   `(defun ,(concatenate-symbols parent-type slot) (type)
      (,(concatenate-symbols struct-name slot) (,(concatenate-symbols parent-type struct-name) type))))
-
-
-(defmacro create-struct-type (name)
-  (let ((map-name (intern (string-upcase (concatenate 'string name "-types"))))
-	(put-name (intern (string-upcase (format nil "put-~a-type" name))))
-	(get-name (intern (string-upcase (format nil "get-~a-type" name)))))
-    `(let ((,map-name (make-hash-table)))
-       (defun ,put-name (name content)
-	 (setf (gethash name ,map-name) content))
-       (defun ,get-name (name)
-	 (gethash name ,map-name)))))
-	
