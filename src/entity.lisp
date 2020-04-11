8e3624da9d967f5ff1a3f6abc71f389b10b7e387
matt <->
2020-04-11 18:40:03 +0200

    F : Added basic player inventory slots

--- src/entity.lisp
+++ src/entity.lisp
@@ -1,14 +1,27 @@
 (in-package :rl)
 
 (defstruct entity
+  (name "entity")
   entity-type
   position
   (behavior 'random)
+  (health 100)
   (speed 10))
 
 (defstruct-type entity
   graphic
   color-fore)
+
+(defstruct (player (:include entity))
+  gold
+  slot-weapon
+  slot-head
+  slot-armor
+  slot-boots
+  slot-ring
+  (has-orb nil))
+  
+  
 
   
 (defun setup-entity-types ()
@@ -18,7 +31,8 @@
 					     :color-fore :white)))
 
 (defun make-player-new ()
-  (make-entity :entity-type (lookup-entity-type 'player)
+  (make-player :name "Rincewind"
+	       :entity-type (lookup-entity-type 'player)
 	       :position (cons 2 3)))
 
 
@@ -33,3 +47,13 @@
 	 (map (world-current-map *world*)))
     (move-entity-delta map entity dx dy)))
 	   
+(defun draw-player-stats (player)
+  (let ((x (- *terminal-width* 15)))
+    (set-string x 1 (entity-name player))
+    (set-string x 3 (format nil "HP : ~a" (player-health player)))
+    (set-string x 4 (format nil "Speed : ~a" (player-speed player)))
+    (set-string x 6 (format nil "Weapon : ~a" (player-slot-weapon player)))
+    (set-string x 7 (format nil "Head : ~a" (player-slot-head player)))
+    (set-string x 8 (format nil "Armor : ~a" (player-slot-armor player)))
+    (set-string x 9 (format nil "Boots : ~a" (player-slot-boots player)))
+    (set-string x 10 (format nil "Ring : ~a" (player-slot-ring player)))))
--- src/world.lisp
+++ src/world.lisp
@@ -24,23 +24,25 @@
 			 
 
 (defun redraw-world (world x y width height)
-  "Redraws the current map, starting at (x,y), cropped to (width,height)"
-  (let* ((map (world-current-map world))
-	 (offset (world-offset world)))
-    (iterate-map map (lambda (map-x map-y id-x id-y)
-		       (let* ((cell (get-cell map map-x map-y)))
-			 (when cell
-			   (let ((graphic (map-cell-graphic cell))
-				 (color-pair (list (map-cell-color-fore cell) (map-cell-color-back cell)))
-				 (entity (map-cell-entity cell)))
-			     (when entity
-			       (setf graphic (entity-graphic entity))
-			       (setf (first color-pair) (entity-color-fore entity)))
-			     (set-char id-x id-y graphic color-pair)))))
-		 (+ x (car offset)) (+ y (cdr offset)) width height)
-    ;;(draw-entity (world-player world))
-    (status-line-draw)
-    (set-string 0 0 (format nil "Timer : ~a" *timer*))))
+  "Redraws the current map, status messages, and player stats starting at (x,y), cropped to (width,height)"
+  (redraw-map (world-current-map world) x y width height (world-offset world))
+  (status-line-draw)
+  (set-string 0 0 (format nil "Timer : ~a" *timer*))
+  (draw-player-stats (world-player world)))
+
+(defun redraw-map (map x y width height offset)
+  "Draws the current map starting at (x,y), cropped to (width,height)"
+  (iterate-map map (lambda (map-x map-y id-x id-y)
+		     (let* ((cell (get-cell map map-x map-y)))
+		       (when cell
+			 (let ((graphic (map-cell-graphic cell))
+			       (color-pair (list (map-cell-color-fore cell) (map-cell-color-back cell)))
+			       (entity (map-cell-entity cell)))
+			   (when entity
+			     (setf graphic (entity-graphic entity))
+			     (setf (first color-pair) (entity-color-fore entity)))
+			   (set-char id-x id-y graphic color-pair)))))
+	       (+ x (car offset)) (+ y (cdr offset)) width height))
 
 (defun world-adjust-offset (world pos w h &optional (gutter 5))
   "Set the map offset for subwindow size (w,h) such that pos remains within gutter of the edges"
